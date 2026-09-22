b948c7466213032cbf36beadf8b7bb396e563e54
Monidip Das <monidipd01@gmail.com>
2026-09-22 08:05:13 +0530

    Upgraded

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 func healthz(w http.ResponseWriter, r *http.Request) {
@@ -14,10 +15,36 @@ func healthz(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("GET/healthz", healthz)
-	err := http.ListenAndServe(":8080", nil)
+	// using mux (multiplexer)
+	// http.HandleFunc("GET /healthz", healthz)
+	// http.HandleFunc("GET /healthz", healthz)
+	// err := http.ListenAndServe(":8080", nil)
+	// err := http.ListenAndServe(":8090", mux)
+	// if err != nil {
+	// 	log.Fatalf("Server Failed : %v", err)
+	// }
+
+	// mux := http.NewServeMux()
+	// mux.HandleFunc("GET /healthz", healthz)
+	// err := http.ListenAndServe(":8090", mux)
+	// if err != nil {
+	// 	log.Fatalf("Server Failed : %v", err)
+	// }
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /healthz", healthz)
+
+	srv := &http.Server{
+		Addr:         ":8090",
+		Handler:      mux,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 30,
+		IdleTimeout:  time.Second * 60,
+	}
+
+	log.Println("Starting server on :8090...")
+	err := srv.ListenAndServe()
 	if err != nil {
 		log.Fatalf("Server Failed : %v", err)
 	}
-
 }
